388c0bb39e7ea75dc5d578cf5b3b02fdf5ca32e3
Sergey <sergeyd@sergeyd.local>
2026-05-29 11:12:31 -0400

    Bracketo live

--- generate_alt_html.sh
+++ generate_alt_html.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
-for src in alt/*.bracket; do
+for src in *.bracket; do
+  case "$src" in
+    sample.bracket)
+      continue
+      ;;
+  esac
   out="${src%.bracket}.html"
   go run braketo.go "$src" > "$out"
 done
